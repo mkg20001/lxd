36fb60c678420f3ab307db0f0449ef9180daadd6
Maciej Krüger <mkg20001@gmail.com>
2021-04-02 08:03:37 +0200

    feat: borg library

---
+++ lxd/storage/borg/utils.go
@@ -0,0 +1,15 @@
+package borg
+
+import "github.com/lxc/lxd/shared"
+
+func IsBorg(config map[string]string) bool {
+	return shared.IsTrue(config["borg.enabled"])
+}
+
+func GetBorgRepo(config map[string]string, volName string) map[string]string {
+	return map[string]string { // TODO: review all is named right
+		"repo": config["borg.repo"] + "/" + volName,
+		"passphrase": config["borg.sshpass"],
+		"key": config["borg.sshkey"],
+	}
+}
